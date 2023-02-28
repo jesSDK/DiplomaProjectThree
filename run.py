4892e5350c8266bdc107bc95edd236273fdd45a1
Jessica Paige <killzone135@gmail.com>
2023-02-28 19:39:41 +0000

    Add: data input and validation

--- run.py
+++ run.py
@@ -12,6 +12,28 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE) #Define our scope, with credentials
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS) #Pass scope and credentials to our client
 SHEET = GSPREAD_CLIENT.open('kuromi_store') #Open our sheet with the client
 
-sales = SHEET.worksheet('sales') #Select the 'sales' worksheet
+def get_sales_data():
+    """
+    Gets the sales figures inputted by the user
+    """
+    print("Please enter the latest sales figures")
+    print("Each value should be seperated by a comma as show below")
+    print("22,33,44,55")
+    data_str = input("Please enter sales figures: ")
 
-data = sales.get_all_values()
+    sales_data = data_str.split(',')
+    validate_sales(sales_data)
+
+def validate_sales(values):
+    """
+    Ensures data is validated before use by checking there are exactly 6 integers provided to us 
+    """
+    try:
+        if len(values) != 6:
+            raise ValueError(
+                f"Expected 6 values, got {len(values)}"
+            )
+    except ValueError as e:
+        print (f"Erorr: {e}")
+
+get_sales_data()
