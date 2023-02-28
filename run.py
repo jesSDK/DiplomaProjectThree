b0bce1089ca35dffdacdfeaa56bdfed15ea58ed8
Jessica Paige <killzone135@gmail.com>
2023-02-28 18:40:12 +0000

    Add: gspead and gauth libraries and configure APIs

--- run.py
+++ run.py
@@ -1,3 +1,17 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import gspread
+from google.oauth2.service_account import Credentials #Import google auth and sheets libraries
+
+SCOPE = [ 
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ] #scope for our APIs
+
+CREDS = Credentials.from_service_account_file('creds.json') #Define our credentials
+SCOPED_CREDS = CREDS.with_scopes(SCOPE) #Define our scope, with credentials
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS) #Pass scope and credentials to our client
+SHEET = GSPREAD_CLIENT.open('kuromi_store') #Open our sheet with the client
+
+sales = SHEET.worksheet('sales') #Select the 'sales' worksheet
+
+data = sales.get_all_values()
